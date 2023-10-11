9d655606153639a6e00c605606d32ac69adcab78
yhutter <hutteryannick@gmail.com>
2023-10-11 18:30:33 +0200

    Finish sent token 02 and fix generated text file.

--- group_projects/sent_token_02.py
+++ group_projects/sent_token_02.py
@@ -4,15 +4,17 @@ with open('./verfassung.txt', 'r') as f:
     content = f.read()
 
 # Remove new lines and other stuff
-content.replace("\r\n", "").replace("\n", "")
-content.replace("  ", "")
+content = content.replace("  ", "").replace("\n", "")
 default_st = nltk.sent_tokenize
 sentences_default_st = default_st(text=content)
 print(f"Got {len(sentences_default_st)} sentences with default tokenizer")
+print(sentences_default_st[:5])
 
-SENTENCE_TOKENS_PATTERN = r'w\.'
+SENTENCE_TOKENS_PATTERN = r'[a-zA-Z]\.|\d{4}\.'
 regex_st = nltk.tokenize.RegexpTokenizer(
             pattern=SENTENCE_TOKENS_PATTERN,
+            discard_empty = False,
             gaps=True) 
-sample_sentences = regex_st.tokenize(content)
-print(f"Got {len(sentences_default_st)} sentences with regex tokenizer")
+sentences_regex_st = regex_st.tokenize(content)
+print(f"Got {len(sentences_regex_st)} sentences with regex tokenizer")
+print(sentences_regex_st[:5])
--- group_projects/web_scraping.py
+++ group_projects/web_scraping.py
@@ -1,7 +1,18 @@
 import requests
+import re
+from bs4 import BeautifulSoup
+
+def strip_html_tags(text):
+    soup = BeautifulSoup(text, "html.parser")
+    [s.extract() for s in soup(['iframe', 'script'])]
+    stripped_text = soup.get_text()
+    stripped_text = re.sub(r'[\r|\n|\r\n]+', '\n', stripped_text)
+    return stripped_text
+
 
 data = requests.get('http://www.verfassungen.ch/graubuenden/verf2003.htm')
 content = data.text
+clean_content = strip_html_tags(content)
 
 with open('./verfassung.txt', 'w') as f:
-    f.writelines(content)
+    f.writelines(clean_content)
