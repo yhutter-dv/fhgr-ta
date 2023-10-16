21a181177f6897c7ac2dfeb707d96cde496af97a
yhutter <hutteryannick@gmail.com>
2023-10-16 14:24:12 +0200

    Add print message for bag of words.

--- group_projects/feature_engineering_bag_of_words.py
+++ group_projects/feature_engineering_bag_of_words.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     cv_matrix = cv_matrix.toarray()
     vocab = cv.get_feature_names_out()
     df = pd.DataFrame(cv_matrix, columns=vocab)
+    print(content)
     print(df)
