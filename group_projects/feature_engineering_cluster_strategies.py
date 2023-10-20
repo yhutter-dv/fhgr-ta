f4c1b50657adc3d420697888ad550b7db44ac3bd
yhutter <hutteryannick@gmail.com>
2023-10-20 10:26:13 +0200

    Show most similar documents.

--- group_projects/feature_engineering_cluster_strategies.py
+++ group_projects/feature_engineering_cluster_strategies.py
@@ -7,10 +7,23 @@ from scipy.spatial.distance import squareform
 from scipy.cluster.hierarchy import dendrogram, linkage
 import matplotlib.pyplot as plt
 
+def print_most_similiar_documents(df, number_of_documents = 1):
+    df_sorted = df.sort_values(by="Distance", ascending=True)
+    for i in range(number_of_documents):
+        first_document_index = int(df_sorted["Document\Cluster 1"].values[0])
+        second_document_index = int(df_sorted["Document\Cluster 2"].values[0])
+        first_document = content[first_document_index]
+        second_document = content[second_document_index]
+        print(f"Iteration {i}")
+        print(f"Document #{first_document_index}")
+        print(first_document)
+        print(f"Document #{second_document_index}")
+        print(second_document)
+
 if __name__ == "__main__":
     with open("./newscrawl_2012_10K.txt", "r") as f:
         content = f.readlines()
-    content = normalize_corpus(content[:5], html_stripping = False, contraction_expansion = False)
+    content = normalize_corpus(content[:1000], html_stripping = False, contraction_expansion = False)
 
     tv = TfidfVectorizer(min_df=0., max_df=1., norm='l2',
                      use_idf=True, smooth_idf=True)
@@ -36,14 +49,16 @@ if __name__ == "__main__":
     print(f"Complete Linkage Result")
     print("=" * segments)
     print(df_linkage_complete)
+    print_most_similiar_documents(df_linkage_complete, 5)
 
     print("=" * segments)
     print(f"Average Linkage Result")
     print("=" * segments)
     print(df_linkage_average)
+    print_most_similiar_documents(df_linkage_average, 5)
 
     print("=" * segments)
     print(f"Weighted Linkage Result")
     print("=" * segments)
     print(df_linkage_weighted)
-
+    print_most_similiar_documents(df_linkage_weighted, 5)
