0be128d34c863d9af6138902ec3ff69ece03f092
yhutter <hutteryannick@gmail.com>
2023-11-25 07:54:08 +0100

    NER WIP

--- group_projects/normalize_corpus.py
+++ group_projects/normalize_corpus.py
@@ -6,11 +6,12 @@ import unicodedata
 from nltk.tokenize.toktok import ToktokTokenizer
 from contractions import CONTRACTION_MAP
 
+
 def normalize_corpus(corpus, html_stripping=True, contraction_expansion=True,
-                     accented_char_removal=True, text_lower_case=True, 
-                     text_lemmatization=True, special_char_removal=True, 
+                     accented_char_removal=True, text_lower_case=True,
+                     text_lemmatization=True, special_char_removal=True,
                      stopword_removal=True, remove_digits=True, lang="german"):
-    
+
     normalized_corpus = []
     if lang == "german":
         nlp = spacy.load('de_dep_news_trf')
@@ -26,32 +27,34 @@ def normalize_corpus(corpus, html_stripping=True, contraction_expansion=True,
         # remove accented characters
         if accented_char_removal:
             doc = remove_accented_chars(doc)
-        # expand contractions    
+        # expand contractions
         if contraction_expansion:
             doc = expand_contractions(doc)
-        # lowercase the text    
+        # lowercase the text
         if text_lower_case:
             doc = doc.lower()
         # remove extra newlines
-        doc = re.sub(r'[\r|\n|\r\n]+', ' ',doc)
+        doc = re.sub(r'[\r|\n|\r\n]+', ' ', doc)
         # lemmatize text
         if text_lemmatization:
             doc = lemmatize_text(doc, nlp)
-        # remove special characters and\or digits    
+        # remove special characters and\or digits
         if special_char_removal:
-            # insert spaces between special characters to isolate them    
+            # insert spaces between special characters to isolate them
             special_char_pattern = re.compile(r'([{.(-)!}])')
             doc = special_char_pattern.sub(" \\1 ", doc)
-            doc = remove_special_characters(doc, remove_digits=remove_digits)  
+            doc = remove_special_characters(doc, remove_digits=remove_digits)
         # remove extra whitespace
         doc = re.sub(' +', ' ', doc)
         # remove stopwords
         if stopword_removal:
-            doc = remove_stopwords(doc, is_lower_case=text_lower_case, stopwords=stopwords)
-            
+            doc = remove_stopwords(
+                doc, is_lower_case=text_lower_case, stopwords=stopwords)
+
         normalized_corpus.append(doc)
-        
+
     return normalized_corpus
+
 
 def strip_html_tags(text):
     soup = BeautifulSoup(text, "html.parser")
@@ -60,40 +63,51 @@ def strip_html_tags(text):
     stripped_text = re.sub(r'[\r|\n|\r\n]+', '\n', stripped_text)
     return stripped_text
 
+
 def remove_accented_chars(text):
-    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    text = unicodedata.normalize('NFKD', text).encode(
+        'ascii', 'ignore').decode('utf-8', 'ignore')
     return text
 
+
 def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
-    
+
     # HR: hier wird mit einem compilierten Pattern gearbeitet; dieses wird aus allen Contraction-Schlüsseln zusammengesetzt
-    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), flags=re.IGNORECASE|re.DOTALL)
-    
+    contractions_pattern = re.compile('({})'.format(
+        '|'.join(contraction_mapping.keys())), flags=re.IGNORECASE | re.DOTALL)
+
     # HR: innere Funktion
     # HR: Achtung - die Sarkar-Funktion arbeitet nicht immer fehlerfrei. Hier (hoffentlich) korrigiert.
     def expand_match(contraction):
         match = contraction.group(0)
         # Contraction-Muster sind alle kleingeschrieben
-        expanded_contraction = contraction_mapping.get(match) if contraction_mapping.get(match) else contraction_mapping.get(match.lower())                     
+        expanded_contraction = contraction_mapping.get(match) if contraction_mapping.get(
+            match) else contraction_mapping.get(match.lower())
         # Grossschreibung wiederherstellen
         if match[0].isupper():
-            expanded_contraction = expanded_contraction[0].upper() + expanded_contraction[1:]
+            expanded_contraction = expanded_contraction[0].upper(
+            ) + expanded_contraction[1:]
         return expanded_contraction
-        
-    expanded_text = contractions_pattern.sub(expand_match, text)  # expand match wird für jeden match aufgerufen
+
+    # expand match wird für jeden match aufgerufen
+    expanded_text = contractions_pattern.sub(expand_match, text)
     expanded_text = re.sub("'", "", expanded_text)  # noch etwas aufräumen
     return expanded_text
 
+
 def lemmatize_text(text, nlp):
     text = nlp(text)
-    text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+    text = ' '.join([word.lemma_ if word.lemma_ !=
+                    '-PRON-' else word.text for word in text])
     return text
+
 
 def remove_special_characters(text, remove_digits=False):
     pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
     text = re.sub(pattern, '', text)
     text = text.replace("\t", "")
     return text
+
 
 def remove_stopwords(text, is_lower_case=False, stopwords=[]):
     tokenizer = ToktokTokenizer()
@@ -102,13 +116,16 @@ def remove_stopwords(text, is_lower_case=False, stopwords=[]):
     if is_lower_case:
         filtered_tokens = [token for token in tokens if token not in stopwords]
     else:
-        filtered_tokens = [token for token in tokens if token.lower() not in stopwords]
-    filtered_text = ' '.join(filtered_tokens)    
+        filtered_tokens = [
+            token for token in tokens if token.lower() not in stopwords]
+    filtered_text = ' '.join(filtered_tokens)
     return filtered_text
+
 
 if __name__ == "__main__":
     with open('./verfassung.txt', 'r') as f:
         content = f.readlines()
 
-    normalized = normalize_corpus(content, html_stripping = False, contraction_expansion = False)
+    normalized = normalize_corpus(
+        content, html_stripping=False, contraction_expansion=False)
     print(normalized[:10])
